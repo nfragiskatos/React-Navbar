284bda2e912a0852063781cf8ab92099f04cb57b
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-25 10:32:54 -0700

    Show links toggle
    - Added state variable on whether or not to show the links
    - Added onClick for the toggle button
    - Added string interpolation for the links container to dynamically set the 'show-container' class based off the showLinks state variable.

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,16 +4,18 @@ import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+	const [ showLinks, setShowLinks ] = useState(false);
+
 	return (
 		<nav>
 			<div className="nav-center">
 				<div className="nav-header">
 					<img src={logo} alt="logo" />
-					<button className="nav-toggle">
+					<button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
 						<FaBars />
 					</button>
 				</div>
-				<div className="links-container show-container">
+				<div className={`links-container ${showLinks && 'show-container'}`}>
 					<ul className="links">
 						{links.map((link) => {
 							const { id, url, text } = link;
